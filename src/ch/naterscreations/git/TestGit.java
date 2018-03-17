9a74cbe79ec210b78ff8d1371f246a87f118b426
Quentin Nater <Quentin Nater@10.91.133.50>
2018-03-17 11:46:26 +0100

    Created first class on git

---
+++ src/ch/naterscreations/git/TestGit.java
@@ -0,0 +1,13 @@
+package ch.naterscreations.git;
+
+public class TestGit
+{
+	@SuppressWarnings("unused")
+	private String gitFromScratch;
+	
+	public TestGit(String parametre)
+	{
+		this.gitFromScratch = parametre;		
+	}
+	
+}
