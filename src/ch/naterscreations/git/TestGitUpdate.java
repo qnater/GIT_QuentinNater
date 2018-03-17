13db8ad83fce69e8fd8fbad76907d75a49a5cb64
favre <favre@10.91.130.221>
2018-03-17 13:01:51 +0100

    TestGitUpdate

---
+++ src/ch/naterscreations/git/TestGitUpdate.java
@@ -0,0 +1,10 @@
+package ch.naterscreations.git;
+
+public class TestGitUpdate extends TestGit {
+
+	public TestGitUpdate(String parametre) {
+		super(parametre);
+		// TODO Auto-generated constructor stub
+	}
+
+}
