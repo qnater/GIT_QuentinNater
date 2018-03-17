ceb8089445f8c4be007d3e01aa2c3b6e4f40503b
Quentin Nater <Quentin Nater@10.91.133.50>
2018-03-17 12:50:47 +0100

    Developer Change Owner

---
+++ src/ch/naterscreations/git/Main.java
@@ -0,0 +1,11 @@
+package ch.naterscreations.git;
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		TestGit myFirstTestGit = new TestGit("My Paramater");
+		System.out.println(myFirstTestGit.returnString());
+	}
+
+}
