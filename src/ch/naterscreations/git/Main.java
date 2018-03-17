d524c9e215d95a729daac8028176b89417d1888e
favre <favre@10.91.130.221>
2018-03-17 13:04:56 +0100

    Rajout d'un message encore

--- src/ch/naterscreations/git/Main.java
+++ src/ch/naterscreations/git/Main.java
@@ -6,7 +6,7 @@ public class Main {
 	{
 		TestGit myFirstTestGit = new TestGit("Owner Paramater");
 		System.out.println(myFirstTestGit.returnString());
-		
+		System.out.println("Merde ça plante pas");
 		System.out.println("Coucou Quentin !");
 		System.out.println("Coucou 2");
 	}
